228fef146ae6cbaabbe11ef7645776ded9b90a40
thebestpatrick <emmanuel.goldstien@gmail.com>
2017-06-29 17:48:07 -0600

    Setting up header and scale reader

---
+++ scalereads.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Reading in data from the scales, expandable to the moisture sensors. 
+
+This module will be responsible for all reading in data from the serial ports on the Raspberry Pi.
+"""
+
+# Import all main modules
+
+# Import all third party modules
+
+# Import all custom modules
+
+__author__ = "Patrick Ingham"
+__copyright__ = "Copyright 2017, Patrick Ingham"
+__credits__ = ["Patrick Ingham", "Stillwater Labs"]
+__license__ = "GPL3"
+__version__ = "0.0.1"
+__maintainer__ = "Patrick Ingham"
+__email__ = "patrickingham@cihenterprises.com"
+__status__ = "testing"
